629cfdb5605ea34b4ee7ad1888b379e586486eee
Tiffany Washke <twashke@gmail.com>
2021-06-13 18:57:37 -0700

    updated index.js server to server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+// Declare npm dependencies
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+const consoleTable = require("console.table");
+const sequelize = require("sequelize");
+
+
+

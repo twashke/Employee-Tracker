ce7df3f499bbdc82d2d3d8d9e75e2c9ec4424f4e
Tiffany Washke <twashke@gmail.com>
2021-06-21 13:03:18 -0700

    deleted files, used other files instead

--- server.js
+++
@@ -1,9 +0,0 @@
-// Declare npm dependencies
-const mysql = require("mysql");
-const inquirer = require("inquirer");
-const chalk = require("chalk");
-const consoleTable = require("console.table");
-const sequelize = require("sequelize");
-
-
-

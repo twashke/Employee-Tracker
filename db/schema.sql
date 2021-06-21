0c4d1f3cd4538f39ff8ff9d308a69a1cc32594dc
Tiffany Washke <twashke@gmail.com>
2021-06-21 13:00:35 -0700

    updated schema for mysql (working)

--- db/schema.sql
+++ db/schema.sql
@@ -4,25 +4,29 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE employeeDepartment (
-    id INT NOT NULL AUTO_INCREMENT,
-    employee_name VARCHAR(30) NOT NULL,
-    primary key(id)
+-- Department Table
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE employeeRole (
-    id INT NOT NULL AUTO_INCREMENT,
-    employee_title VARCHAR(30) NOT NULL,
-    employee_salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
-    primary key (id)
+-- Employee Role Table
+
+CREATE TABLE employee_role (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT
 );
 
-CREATE TABLE employeeInfo (
-    id INT NOT NULL AUTO_INCREMENT,
-    employee_firstName VARCHAR(30) NOT NULL,
-    employee_lastName VARCHAR(30) NOT NULL,
-    employee_role_id INT NOT NULL,
-    employee_manager_id INT NULL,
-    primary key (id)
+-- Employee Table
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    employee_role_id INT,
+    employee_manager_id INT
 );
+
